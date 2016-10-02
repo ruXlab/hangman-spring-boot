1b6bde3199018f55c7cfebfef4f0a8b626468969
ruX <post4ruX@gmail.com>
2016-10-02 15:54:16 +0100

    Show current progress

--- src/main/java/vc/rux/codingtest/hangman/HangmanApplication.java
+++ src/main/java/vc/rux/codingtest/hangman/HangmanApplication.java
@@ -12,5 +12,5 @@ public class HangmanApplication {
 		SpringApplication.run(HangmanApplication.class, args);
 	}
 
-	public static final int MAX_ATTEMPTS = 7;
+	public static final int MAX_ATTEMPTS = 8;
 }
